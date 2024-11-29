164ea6d19ea9a280c636edf9da915d09561dc10d
DariaYashnova1 <128304896+DariaYashnova1@users.noreply.github.com>
2024-11-30 01:28:24 +0300

    Update Main.java

--- lab3/Main.java
+++ lab3/Main.java
@@ -4,12 +4,11 @@ import java.util.List;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Random random = new Random();
-        CheeseShop shop = new CheeseShop(10);
+        CheeseShop shop = new CheeseShop(9);
         Thread shopThread = new Thread(shop);
-        for (int i = 0; i < 10; i++) {
-
+        for (int i = 0; i < 20; i++) {
             Buyer buyer;
             if (random.nextBoolean()){
                 buyer = new HumbleBuyer(shop,"Humble " + i);
@@ -18,8 +17,17 @@ public class Main {
             }
             buyer.start();
         }
+        Thread.sleep(2000);
+
+        shop.printQueue();
 
         shopThread.start();
+        Thread.sleep(5000);
+        Buyer nbuyer = new BraveBuyer(shop,"Brave #"  );
+        nbuyer.start();
+        Thread.sleep(5000);
+        Buyer nbuyer2 = new HumbleBuyer(shop,"Humble *"  );
+        nbuyer2.start();
 
 
     }
