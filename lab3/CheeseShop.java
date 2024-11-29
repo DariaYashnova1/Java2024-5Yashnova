11e99261f82d7a5f6ab652ffb4cf2f50eb33f7eb
DariaYashnova1 <128304896+DariaYashnova1@users.noreply.github.com>
2024-11-30 01:27:29 +0300

    Update CheeseShop.java

--- lab3/CheeseShop.java
+++ lab3/CheeseShop.java
@@ -2,42 +2,66 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.locks.ReentrantLock;
 
 class CheeseShop implements Runnable {
-    private final BlockingQueue<Buyer> queue;
-
+    private BlockingQueue<Buyer> queue;
+    int buyers_num ;
     public CheeseShop(int capacity) {
         this.queue = new ArrayBlockingQueue<>(capacity);
+        buyers_num = capacity;
+    }
+
+    public boolean chBoolInv() throws InterruptedException {
+        Thread.sleep(2000);
+        int humbles = 0;
+        int braves = 0;
+        List<Buyer> queueList = new ArrayList<>(queue);
+        for (Buyer buyer : queueList) {
+            if (buyer instanceof BraveBuyer) {
+                braves++;
+            } else {
+                humbles++;
+            }
+        }
+        return humbles >= 2 && braves >= 2;
     }
 
     public synchronized void checkInv() throws InterruptedException {
-            int humbles = 0;
-            int braves = 0;
-            List<Buyer> queueList = new ArrayList<>(queue);
-            for (Buyer buyer : queueList) {
-                if (buyer instanceof BraveBuyer) {
-                    braves++;
-                } else {
-                    humbles++;
-                }
-            }
-            if (braves < 2) {
-                String name = "Brave " + (int) (Math.random() * 100);
-                BraveBuyer newB = new BraveBuyer(this, name);
-                newB.start();
-            }
-            if (humbles < 2) {
-                String name = "Humble " + (int) (Math.random() * 100);
-                HumbleBuyer newH = new HumbleBuyer(this, name);
-                newH.start();
+        int humbles = 0;
+        int braves = 0;
+        List<Buyer> queueList = new ArrayList<>(queue);
+        for (Buyer buyer : queueList) {
+            if (buyer instanceof BraveBuyer) {
+                braves++;
+            } else {
+                humbles++;
             }
         }
+        if (braves < 2) {
+            String name = "Brave " + (int) (Math.random() * 100);
+            BraveBuyer newB = new BraveBuyer(this, name);
+            newB.start();
+
+        }
+        if (humbles < 2) {
+            String name = "Humble " + (int) (Math.random() * 100);
+            HumbleBuyer newH = new HumbleBuyer(this, name);
+            newH.start();
+
+        }
+
+    }
 
 
-    public void getCheese(Buyer buyer) throws InterruptedException {
+    public synchronized void getCheese(Buyer buyer) throws InterruptedException {
+        if(!emtyPlaces()){
+            resizeQueue();
+        }
         queue.put(buyer);
         System.out.println(buyer.getName() + " get Cheese");
+        printQueue();
+
+        notify();
     }
 
     private void moveBraves() throws InterruptedException {
@@ -62,23 +86,33 @@ class CheeseShop implements Runnable {
         List<Buyer> queueList = new ArrayList<>(queue);
         System.out.println();
         for (Buyer buyer : queueList) {
-            System.out.printf(buyer.getName() + "|");
+            System.out.printf(buyer.getName() + "<-");
         }
         System.out.println();
 
     }
 
     public synchronized void serve() throws InterruptedException {
-            Buyer buyer = queue.take();
-            System.out.println(queue.size());
-            moveBraves();
-            Thread.sleep((long) (Math.random() * 2000)); // Имитация времени обслуживания
-            System.out.println(buyer.getName() + " cheese");
+        Buyer buyer = queue.take();
+        moveBraves();
+        Thread.sleep((long) (Math.random() * 2000)); // Имитация времени обслуживания
+        System.out.println(buyer.getName() + " cheese");
+        if(!chBoolInv()){
             checkInv();
-            printQueue();
+            wait();
+        }
 
     }
+    public boolean emtyPlaces(){
+        return queue.size()<buyers_num;
+    }
 
+    public void resizeQueue(){
+        BlockingQueue<Buyer> newQueue = new ArrayBlockingQueue<>(buyers_num+1); // Новый размер
+        queue.drainTo(newQueue);
+        buyers_num = buyers_num+1;
+        queue = newQueue;
+    }
 
     @Override
     public void run() {
