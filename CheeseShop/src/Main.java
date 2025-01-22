c4db2d4293b18e8b43aab6b35d8c66c5c0a188bc
DariaYashnova1 <128304896+DariaYashnova1@users.noreply.github.com>
2025-01-22 21:59:21 +0300

    Update Main.java

--- CheeseShop/src/Main.java
+++ CheeseShop/src/Main.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Random random = new Random();
-        CheeseShop shop = new CheeseShop(9);
+        CheeseShop shop = new CheeseShop(10);
         Thread shopThread = new Thread(shop);
         for (int i = 0; i < 10; i++) {
             Buyer buyer;
@@ -15,20 +15,19 @@ public class Main {
             }else{
                 buyer = new BraveBuyer(shop,"Brave " + i);
             }
-            buyer.start();
+            shop.getCheese(buyer);
         }
-        //Thread.sleep(2000);
+       // Thread.sleep(2000);
 
         shop.printQueue();
 
         shopThread.start();
         //Thread.sleep(5000);
         Buyer nbuyer = new BraveBuyer(shop,"Brave #"  );
-        nbuyer.start();
+        shop.getCheese(nbuyer);
         //Thread.sleep(5000);
         Buyer nbuyer2 = new HumbleBuyer(shop,"Humble *"  );
-        nbuyer2.start();
-
+        shop.getCheese(nbuyer2);
 
     }
 }
