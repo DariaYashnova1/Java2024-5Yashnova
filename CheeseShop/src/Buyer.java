f2ab10c7723ca8070f6064c52bc5d54644b2082a
DariaYashnova1 <128304896+DariaYashnova1@users.noreply.github.com>
2025-01-23 17:19:09 +0300

    Update Buyer.java

--- CheeseShop/src/Buyer.java
+++ CheeseShop/src/Buyer.java
@@ -10,11 +10,20 @@ abstract class Buyer extends Thread {
     public void run() {
         try {
             // зайти в очередь
+//            shop.enterQueue(this);
+//            System.out.println(getName() + " get Cheese");
             // ждём, когда дойдёт до нас
             synchronized (this){
+                shop.enterQueue(this);
+                System.out.println(getName() + " get Cheese");
                 wait();
             }
-            this.interrupt();
+            shop.leaveQueue();
+            System.out.println(getName() + " cheese");
+            synchronized (shop){
+                shop.notify();
+                System.out.println("Waiting shop not");
+            }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
