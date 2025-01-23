7587c5f2b202003adff8650d2830b15a3b73835b
DariaYashnova1 <128304896+DariaYashnova1@users.noreply.github.com>
2025-01-23 22:04:59 +0300

    Maven structure changed, multithreading logic add

---
+++ CheeseShop/src/main/java/org/yashnova/Buyer.java
@@ -0,0 +1,44 @@
+package org.yashnova;
+
+abstract class Buyer extends Thread {
+    protected final CheeseShop shop;
+
+    public Buyer(CheeseShop shop, String name) {
+        super(name);
+        this.shop = shop;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // зайти в очередь
+            synchronized (this){
+                shop.enterQueue(this);
+                System.out.println(getName() + " get Cheese");
+                notify();
+                wait();
+            }
+            shop.leaveQueue();
+            System.out.println(getName() + " cheese");
+            synchronized (shop){
+                shop.notify();
+                System.out.println("Waiting shop not");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
+
+class BraveBuyer extends Buyer {
+    public BraveBuyer(CheeseShop shop, String name) {
+        super(shop, name);
+    }
+}
+
+class HumbleBuyer extends Buyer {
+    public HumbleBuyer(CheeseShop shop, String name) {
+        super(shop, name);
+    }
+}
